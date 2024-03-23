2208cb3ac72c1cc98221934e9203b314be3d1b05
aryaraul <164596594+aryaraul@users.noreply.github.com>
2024-03-23 14:46:48 +0530

    Update login.js

--- login/login.js
+++ login/login.js
@@ -3,16 +3,14 @@ document.getElementById("login-form").addEventListener("submit", function(event)
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  // Retrieve username and password from local storage
   const savedUsername = localStorage.getItem("username");
   const savedPassword = localStorage.getItem("password");
-
-  // Check if entered credentials match saved credentials
+  
   if (username === savedUsername && password === savedPassword) {
-      // Redirect to dashboard page upon successful login
+
       window.location.href = "dashboard.html";
   } else {
-      // Display an alert for invalid credentials
+
       alert("Invalid username or password. Please try again.");
   }
 });
